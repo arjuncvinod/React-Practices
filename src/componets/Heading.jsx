95d9a2ab2440d74f0759e2b29024711bc3bd6dc4
arjuncvinod <arjuncvinod007@gmail.com>
2023-06-28 22:21:31 +0530

    Added Componets

---
+++ src/componets/Heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading() {
+  return <h1>Fruits</h1>;
+}
+export default Heading;
---
+++ src/componets/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Apple</li>
+      <li>Mango</li>
+      <li>Orange</li>
+    </ul>
+  );
+}
+
+export default List;

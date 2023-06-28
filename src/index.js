57c3e8ab688f610f5424371c98b5854b6d7d886c
Arjun C Vinod <68469520+arjuncvinod@users.noreply.github.com>
2023-06-28 20:52:17 +0530

    Dynamic greetings added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+//practice #1
+
 var React = require("react");
 var ReactDOM = require("react-dom");
+
 const img = "https://picsum.photos/200";
 let a = 1;
 const style = {
@@ -20,3 +23,36 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Practice #2
+
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom";
+let style={
+  color:"red"
+}
+let day="night"
+const date=new Date()
+let hour = date.getHours()
+if(hour<12){
+  day="Morning"
+  style.color="red"
+}else if(hour>=12 && hour<18){
+ day="After Noon"
+ style.color="green"
+}
+else if(hour>=18 && hour<24){
+ day="Night"
+ style.color="blue"
+}
+ReactDOM.render(<h1 className="heading" style={style}>Good {day}</h1>, document.getElementById("root"));

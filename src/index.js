b3645b7efe7bc62a1999f325a72b76c40b20302f
Arjun C Vinod <68469520+arjuncvinod@users.noreply.github.com>
2023-07-01 00:01:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -71,3 +71,41 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
+//---------------------------------------------------------------------------------------------------
+
+//practice #4
+
+//React Props
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function Card(props){
+ return(
+   <div>
+  <h2>{props.name}</h2>
+    <img 
+      src={props.img}
+      alt="avatar_img"
+    />
+    <p>{props.tel}</p>
+    <p >{props.email}</p>
+</div>
+ )
+}
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card  name="Beyonce" img=" https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+    tel="+123 456 789" email="b@beyonce.com"
+    />
+   <Card name="Jack Bauer" img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+   tel="+987 654 321" email="jack@nowhere.com"/>
+</div>
+   ,
+  document.getElementById("root")
+);
+
+
+
+

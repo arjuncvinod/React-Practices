7463b8c405a99f4cab8802d6ed6d5b18f8040a71
Arjun C Vinod <68469520+arjuncvinod@users.noreply.github.com>
2023-07-06 23:08:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -204,8 +204,61 @@ event.preventDefault()
     </div>
   );
 }
-
 export default App;
 
 
+//------------------------------------------------------------------------------------------------------------------------
+//Pracice #8
+
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setFullName(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lname: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
 
